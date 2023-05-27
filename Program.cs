144db721bb68db0deba282d028fb7a0f0789dd09
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-05-28 01:17:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string folderPath = @"C:\Users\ahmet\Desktop\deneme\A'dan Z'ye Entity Framework Core Eğitimi";
+
+        try
+        {
+            string[] fileNames = Directory.GetFiles(folderPath);
+
+            Array.Sort(fileNames); // Dosya isimlerini sırala
+
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+
+                //Console.WriteLine(Path.GetFileName(fileNames[i]));
+                string fileName = fileNames[i];
+                string newFileName = Path.Combine(Path.GetDirectoryName(fileName), (i + 10) + ". " + Path.GetFileName(fileName));
+                File.Move(fileName, newFileName);
+                Console.WriteLine("Dosya adı güncellendi: " + newFileName);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("-----------------------------------------------------------------------------------------");
+                Console.ResetColor();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Hata oluştu: " + e.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
